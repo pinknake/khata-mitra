132de14a1fdbb6b7aa2267a3841a6f554c6beeff
Ankush Saggu <ankush22004@gmail.com>
2026-08-09 22:11:20 +0530

    Add Firebase configuration to fire.js

--- fire.js
+++ fire.js
@@ -1,1 +1,7 @@
-
+// ===== FIREBASE CONFIG =====
+const firebaseConfig = {
+  apiKey: "AIzaSyBbSdkHFFpuWxl0DaXGLmTcAdD3tJv4cjc",
+ authDomain: "khata-mitra-pro.firebaseapp.com",
+  databaseURL: "https://khata-mitra-pro-default-rtdb.asia-southeast1.firebasedatabase.app"
+};
+firebase.initializeApp(firebaseConfig);
